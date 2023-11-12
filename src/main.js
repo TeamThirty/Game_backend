e25c5ea18130550f801b69b4de9126ff8ec5342b
Alexander Shuglaevksii <a.shugalevskii@gmail.com>
2023-11-12 11:41:50 +0700

    selection working. cursor magnetes to walls.

--- src/main.js
+++ src/main.js
@@ -13,15 +13,9 @@ const loader = new MySTLLoader();
 
 let room = new room_box(scene, 20, 20 ,10);
 
-let raycaster;
-let raycaster2;
-
-let INTERSECTED;
 
 const pointer = new THREE.Vector2();
-const radius = 5;
-raycaster = new THREE.Raycaster();
-raycaster2 = new THREE.Raycaster();
+
 
 
 
@@ -32,8 +26,9 @@ function onPointerMove( event )
     pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
+
 const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(-10, 10, -10);
+camera.position.set(10, 10, 10);
 
 let  cursor = new THREE.Mesh(new THREE.SphereGeometry(1, 20, 20), new THREE.MeshBasicMaterial( {color : 0xff00ff}))
 scene.add(cursor)
@@ -64,7 +59,7 @@ const canvas = renderer.domElement;
 document.body.appendChild(canvas);
 renderer.setSize(window.innerWidth, window.innerHeight);
 const controls = new OrbitControls(camera, canvas);
-controls.target.set(0, 5, 0);
+controls.target.set(0, 0, 0);
 controls.update();
 
 //ambient light
@@ -89,51 +84,6 @@ function resize() {
 }
 window.addEventListener('resize', () => { resize() });
 
-var axesHelper = new THREE.AxesHelper(10);
-//scene.add(axesHelper);
-
-function render() {
-
-    raycaster.setFromCamera( pointer, camera );
-
-    const intersects = raycaster.intersectObjects( objList, false );
-
-    if ( intersects.length > 0 ) {
-
-        if ( INTERSECTED != intersects[ 0 ].object ) {
-
-            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-            INTERSECTED = intersects[ 0 ].object;
-            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            INTERSECTED.material.emissive.setHex( 0xff0000 );
-        }
-
-    } else {
-
-        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-        INTERSECTED = null;
-
-    }
-
-    renderer.render(scene, camera);
-}
-
-
-let mousePos = new THREE.Vector3(0,0,0);
-
-
-function animate()
-{   
-    requestAnimationFrame(animate)
-    render()
-    update()
-    
-}
-
-
-
 function onWindowResize() 
 {
 
@@ -145,22 +95,29 @@ function onWindowResize()
 }
 
 
-window.addEventListener( 'resize', onWindowResize );
+let mousePos = new THREE.Vector3(0,0,0);
 
-document.addEventListener( 'mousemove', onPointerMove );
 
 
 let currently_selected;
 let currently_holding = false;
 
+let raycaster;
+let raycaster2;
 
-function update()
+let INTERSECTED;
+let INTERSECTED_WALL;
+
+raycaster = new THREE.Raycaster();
+raycaster2 = new THREE.Raycaster();
+
+
+function raycast_walls()
 {
     raycaster2.setFromCamera( pointer, camera );
     raycaster2.layers.set( 10 ); 
 
     const intersects = raycaster2.intersectObjects( scene.children , false );
-
     let INTERSECTED_point;
     if ( intersects.length > 0 ) {
 
@@ -193,12 +150,37 @@ function update()
 
     if(currently_selected)
     {
-        currently_selected.position.set(cursor.position.x, 0, cursor.position.z)
+        //currently_selected.position.set(cursor.position.x, 0, cursor.position.z)
         //currently_selected.position.copy(mousePos)
     }
 }
 
-function myScript()
+function raycast_objects()
+{
+    raycaster.setFromCamera( pointer, camera );
+    const intersects = raycaster.intersectObjects( objList, false );
+
+    if ( intersects.length > 0 ) {
+
+        if ( INTERSECTED != intersects[ 0 ].object ) {
+
+            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+            INTERSECTED.material.emissive.setHex( 0xff0000 );
+        }
+
+    } else {
+
+        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+        INTERSECTED = null;
+
+    }
+}
+
+function onClick()
 {    
     if (currently_holding)
     {
@@ -218,9 +200,29 @@ function myScript()
     }
 }
 
-document.addEventListener( 'click' , myScript);
-document.addEventListener( 'mouseup', function(){currently_selected = null; console.log("released")} );
+function onMouseUp(){currently_selected = null; console.log("released")} 
 
+
+window.addEventListener( 'resize', onWindowResize );
+
+document.addEventListener( 'mousemove', onPointerMove );
+document.addEventListener( 'click' , onClick);
+document.addEventListener( 'mouseup', onMouseUp);
+
+function render() 
+{
+    renderer.render(scene, camera);
+}
+
+function animate()
+{   
+    requestAnimationFrame(animate)
+    
+    raycast_walls()
+    raycast_objects()    
+    render()
+
+}
 
 if (WebGL.isWebGLAvailable()) {
     animate()
