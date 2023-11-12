38047b90545b1c38bf5fe9ea7b0f0a7d50590abd
Natalia <n.shugalevskaia@gmail.com>
2023-11-12 19:15:04 +0600

    cors again

--- database/init.py
+++ database/init.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*", "allow_headers": "Content-Type"}})
+CORS(app, resources={r"*": {"origins": "*", "allow_headers": "Content-Type"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 app.config['SECRET_KEY'] = Config.SECRET_KEY
